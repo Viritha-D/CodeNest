efc914ae5821f3b14c15225e0c6f2d9753695d73
Viritha Daggubati <145008999+Viritha-D@users.noreply.github.com>
2025-06-07 23:26:06 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>numbers={1,7,8,9,4};
+    for(int num:numbers){
+        cout<<num<<"\n";
+    }
+    return 0;
+
+}
